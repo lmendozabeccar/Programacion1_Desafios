9b438e50b4c5fda5f79f59936781439865b4242b
lmendozabeccar <lmendozabeccar@uade.edu.ar>
2024-08-21 20:22:05 -0300

    falta crud

--- desafio_3.py
+++ desafio_3.py
@@ -2,7 +2,7 @@ productos = [
     [101, "AdidAS", 42, 3],
     [102, "NiKe", 40, 0],
     [103, "PumA", 35, 5],
-    [104, "FIla", 39, 10],
+    [104, "FIla", 39, 9],
     [105, "adidAs", 40, 1],
     [106, "PUma", 44, 2],
     [107, "converse", 41, 3],
@@ -14,7 +14,7 @@ productos = [
 Caracteres_recortados = list()
 for id, marca, talle, stock in productos:
     Caracteres_recortados.append([id, marca[:4], talle, stock])
-print(Caracteres_recortados)
+productos = Caracteres_recortados[:]
 
 #Estandarizar el nombre de las marcas
 for h in range(len(productos)):
@@ -22,3 +22,10 @@ for h in range(len(productos)):
 print(productos)
 
 #Ordenar de forma ascendente por stock y por marca de forma secundaria
+productos = sorted(productos, key=lambda x: (x[3], x[1]))
+
+#Mostrar la lista de forma clara
+print(f"|{"id":^5}|{"marca":^7}|{"talle":^7}|{"stock":^7}|")
+for id, marca, talle, stock in productos:
+    print(f"|{id:^5}|{marca:^8}|{talle:^6}|{stock:^5}|")
+
